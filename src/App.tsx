2cb22c2c3b9fa3beee535965cd3b012c1ed054b4
Enes Gonultas <enesg@pm.me>
2023-08-02 08:07:11 +0300

    Rendered data from json (part 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import * as React from 'react';
-import './style.css';
-import Grid from './grid';
-import dataList from './data.json';
+import * as React from "react";
+import "./style.css";
+import Grid from "./grid";
+import dataList from "./data.json";
 
 function control(today: Date, limit: number) {}
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <h1>Dgpays Case Study </h1>
-      {/* <Grid source={sourceProp} />  */}
+      <Grid source={sourceProp} />
     </div>
   );
 }
--- src/grid.tsx
+++ src/grid.tsx
@@ -1,3 +1,33 @@
-export default function Grid() {
-  return false;
+interface Solution {
+  name: string;
+  mailReceivedDate: string;
+  solutionSentDate?: string;
+  isBackgroundColorRed?: boolean;
+}
+
+export interface Props {
+  source: Array<Solution>;
+}
+
+export default function Grid({ source }: Props) {
+  return (
+    <table>
+      <tbody>
+        {source.map(
+          ({
+            name,
+            mailReceivedDate,
+            solutionSentDate,
+            isBackgroundColorRed,
+          }) => (
+            <tr style={isBackgroundColorRed ? { backgroundColor: "red" } : {}}>
+              <td>{name}</td>
+              <td>{mailReceivedDate}</td>
+              <td>{solutionSentDate}</td>
+            </tr>
+          )
+        )}
+      </tbody>
+    </table>
+  );
 }
